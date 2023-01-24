42a9b85d43fc65448baf4d6cd4a15c8219d24a31
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-24 16:21:17 -0300

    🚧 feat: concluiu o desafio do curso de formulários

--- src/app/challenge/project-form/project-form.component.ts
+++ src/app/challenge/project-form/project-form.component.ts
@@ -5,48 +5,53 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-project-form',
   templateUrl: './project-form.component.html',
-  styleUrls: ['./project-form.component.css']
+  styleUrls: ['./project-form.component.css'],
 })
 export class ProjectFormComponent implements OnInit {
-
-
   challengeForm: FormGroup;
 
-  constructor() { }
+  projectStatus: string = 'Estável';
+
+  constructor() {}
 
   ngOnInit(): void {
-
     this.challengeForm = new FormGroup({
-      'name': new FormControl(null, [Validators.required, this.validatorName], null),
-      'email': new FormControl(null, [Validators.email, Validators.required], this.asyncValidatorName),
-    })
+      'name': new FormControl(
+        null,
+        [Validators.required, this.validatorName],
+        null
+      ),
+      'email': new FormControl(
+        null,
+        [Validators.email, Validators.required],
+        this.asyncValidatorName
+      ),
+      'projectStatus': new FormControl(null)
+    });
   }
 
   submit() {
-
+    console.log(this.challengeForm.value)
   }
 
-
-  validatorName(control: FormControl): {[s: string] : boolean} {
-
-    if(control.value === "Test") {
-      return {'nameIsNotPermited': true}
+  validatorName(control: FormControl): { [s: string]: boolean } {
+    if (control.value === 'Test') {
+      return { 'nameIsNotPermited': true };
     }
     return null;
   }
 
-  asyncValidatorName(control:  FormControl): Promise<any> | Observable<any> {
-
-    const promise = new Promise((resolve, reject)=> {
-
-      if(control.value === "Test") {
-        resolve({'nameIsNotPermited': true})
-      } else {
-        resolve(null)
-      }
-    })
+  asyncValidatorName(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({ 'nameIsNotPermited': true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
 
     return promise;
   }
-
 }
